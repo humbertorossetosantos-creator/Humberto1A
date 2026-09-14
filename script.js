804f3cc9e94272cbf3d7a7c39ab3a144b22c3bdc
humbertorossetosantos-creator <humberto.rosseto.santos@escola.pr.gov.br>
2026-09-14 09:32:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-
+  const botaoes = document.querySelector("button");
+    botoes.
+    botoes.forEach(function(botao){
+    botoes.addEventListener("click",botaoClicado);
+    function botaoClicado() { 
+      console.log("fui clicado");
+      let texto = botao.querySelector("span");
+      if (curtiu === false){
+      }
+       texto.textContent++;}
+    
+    });
